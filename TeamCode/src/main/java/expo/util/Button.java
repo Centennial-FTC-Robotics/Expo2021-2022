6c66e57cdf8941c9e37d4ea5525c07c2e6db6749
LuqDude <luqmanforge@gmail.com>
2021-09-21 14:12:53 -0400

    Added Subsystem which every part of the robot (intake, drivetrain etc) will implement Added Robot which can hold all the subsystems to init them all at once Added Button which is just an enum for every controller button Added ExpoGamepad which is just the regular gamepad Added ButtonToggle which ExpoGamepad can use for a built in toggle so teleop opmodes dont have to Added Vector which can be used for controller inputs and positions and makes stuff like field centric easy cus you can rotate it

---
+++ TeamCode/src/main/java/expo/util/Button.java
@@ -0,0 +1,18 @@
+package expo.util;
+
+public enum Button {
+    A,
+    B,
+    X,
+    Y,
+    START,
+    BACK,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UP,
+    LEFT_BUMPER,
+    RIGHT_BUMPER,
+    LEFT_STICK,
+    RIGHT_STICK
+}
