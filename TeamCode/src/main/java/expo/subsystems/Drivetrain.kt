5da8afb5f6db54997fd605d709e31c7de048bc5c
Danylo <danylovoloshyn0@gmail.com>
2021-10-25 19:04:08 -0400

    basic drivetrain stuff

--- TeamCode/src/main/java/expo/subsystems/Drivetrain.kt
+++ TeamCode/src/main/java/expo/subsystems/Drivetrain.kt
@@ -1,19 +1,27 @@
 package expo.subsystems
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.hardware.DcMotor
 import expo.Subsystem
 
 class Drivetrain : Subsystem {
     //TODO: danylo
-
+    private lateinit var frontLeft: DcMotor
+    private lateinit var frontRight: DcMotor
+    private lateinit var backLeft: DcMotor
+    private lateinit var backRight: DcMotor
 
     override fun initialize(opMode: LinearOpMode) {
-        //add ur init code here
+        frontLeft = opMode.hardwareMap.dcMotor.get("frontLeft")
+        frontRight = opMode.hardwareMap.dcMotor.get("frontRight")
+        backLeft = opMode.hardwareMap.dcMotor.get("backLeft")
+        backRight = opMode.hardwareMap.dcMotor.get("backRight")
     }
 
     public fun setMotorPowers(frontLeft: Double, frontRight: Double, backLeft: Double, backRight: Double) {
-        //set the motor powers
+        this.frontLeft.power = frontLeft
+        this.frontRight.power = frontRight
+        this.backLeft.power = backLeft
+        this.backRight.power = backRight
     }
-
-
 }
