279a2e8d5eab7b35dc9cdb7b799ec98ad3f0aed0
LuqDude <luqmanforge@gmail.com>
2021-11-02 14:18:30 -0400

    Moved CarouselSpinner.kt into subsystems

--- TeamCode/src/main/java/expo/subsystems/CarouselSpinner.kt
+++ TeamCode/src/main/java/expo/subsystems/CarouselSpinner.kt
@@ -1,16 +1,17 @@
 package expo.subsystems
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.hardware.DcMotor
 import expo.Subsystem
 
 class CarouselSpinner : Subsystem {
-    //TODO: aman
-
+    private lateinit var carousel: DcMotor
     override fun initialize(opMode: LinearOpMode) {
+        carousel = opMode.hardwareMap.dcMotor.get("carouselMotor")
 
     }
 
-    public fun setPower(power: Double) {
-
+    fun setPower(power: Double) {
+        carousel.power = power
     }
 }
