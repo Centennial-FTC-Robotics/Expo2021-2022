536f66a79bdaece5def027265f3c798dfbf8e8de
LuqDude <luqmanforge@gmail.com>
2021-11-02 07:21:22 -0400

    -Fixed up Carousel Spinner

--- TeamCode/src/main/java/expo/util/CarouselSpinner.kt
+++ TeamCode/src/main/java/expo/util/CarouselSpinner.kt
@@ -2,15 +2,16 @@ package expo.util
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.DcMotor
+import expo.Subsystem
 
-class CarouselSpinner {
+class CarouselSpinner : Subsystem {
     private lateinit var carousel: DcMotor
-    fun initialize(opMode: LinearOpMode) {
+    override fun initialize(opMode: LinearOpMode) {
         carousel = opMode.hardwareMap.dcMotor.get("carouselMotor")
 
     }
 
-    public fun setPower(power: Double) {
+    fun setPower(power: Double) {
         carousel.power = power
     }
 }
