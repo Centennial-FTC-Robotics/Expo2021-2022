22acff407a64bbf3ba4abc07512f86d61bd319e5
LuqDude <luqmanforge@gmail.com>
2021-11-09 08:31:54 -0500

    -Merged PIDController branch and Commands branch into master

--- TeamCode/src/main/java/expo/Robot.kt
+++ TeamCode/src/main/java/expo/Robot.kt
@@ -1,19 +1,18 @@
 package expo
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import expo.subsystems.CarouselSpinner
-import expo.subsystems.Drivetrain
-import expo.subsystems.Intake
-import expo.subsystems.MotorTester
+import expo.subsystems.*
 
 class Robot {
     val drivetrain = Drivetrain()
     val intake = Intake()
     val spinner = CarouselSpinner()
-    val IMU = expo.subsystems.IMU()
+    val IMU = IMU()
+    val openCV = OpenCV()
+    val outtake = Outtake()
     val motorTester = MotorTester()
 
-    private val subsystems = emptyList<Subsystem>()
+    private val subsystems = listOf(drivetrain, intake, spinner, IMU, openCV, outtake)
 
     fun initialize(opMode: LinearOpMode) {
         for (subsystem: Subsystem in subsystems)
---
+++ TeamCode/src/main/java/expo/subsystems/Outtake.kt
@@ -0,0 +1,9 @@
+package expo.subsystems
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import expo.Subsystem
+
+class Outtake : Subsystem {
+    override fun initialize(opMode: LinearOpMode) {
+    }
+}
