49ff10c8be07632db67be71526742df2b94a51be
LuqDude <luqmanforge@gmail.com>
2021-11-13 21:27:00 -0500

    -Made robot a singleton instance without breaking compatibility

--- TeamCode/src/main/java/expo/util/ExpoOpMode.kt
+++ TeamCode/src/main/java/expo/util/ExpoOpMode.kt
@@ -8,13 +8,13 @@ abstract class ExpoOpMode : LinearOpMode() {
     protected lateinit var controller2: ExpoGamepad
     protected lateinit var robot: Robot
     override fun runOpMode() {
+        robot = Robot
         Robot.motorTester.setMotorName("intake test")
         Robot.motorTester.initialize(this)
         Robot.initialize(this)
         controller1 = ExpoGamepad(gamepad1)
         controller2 = ExpoGamepad(gamepad2)
         waitForStart()
-        robot = Robot
     }
 
     protected fun updateGamepads() {
