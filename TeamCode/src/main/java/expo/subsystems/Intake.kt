d57a39d194e0c01a8e115a45a461508a3dcc20b7
LuqDude <luqmanforge@gmail.com>
2021-11-18 19:41:55 -0500

    -simple intake for now, waiting for danylo

--- TeamCode/src/main/java/expo/subsystems/Intake.kt
+++ TeamCode/src/main/java/expo/subsystems/Intake.kt
@@ -1,11 +1,26 @@
 package expo.subsystems
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.Servo
 import expo.Subsystem
 
 class Intake : Subsystem {
-    //TODO: john
+    lateinit var intakeServo: Servo
+    lateinit var intakeMotor: DcMotor
     override fun initialize(opMode: LinearOpMode) {
+        intakeServo = opMode.hardwareMap.servo.get("intake servo")
+        intakeMotor = opMode.hardwareMap.dcMotor.get("intake motor")
+        intakeMotor.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        intakeServo.direction = Servo.Direction.REVERSE
+        intakeServo.scaleRange(.55, 1.0)
+    }
 
+    fun setIntakePower(power: Double) {
+        intakeMotor.power = power
+    }
+
+    fun setIntakePosition(position: Double) {
+        intakeServo.position = position
     }
 }
