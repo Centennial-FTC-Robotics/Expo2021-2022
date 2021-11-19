e5aa3f0582cd3ad581e8549af07d54123eb56ffb
LuqDude <luqmanforge@gmail.com>
2021-11-18 19:42:26 -0500

    -Linkage part of outtake

--- TeamCode/src/main/java/expo/subsystems/Outtake.kt
+++ TeamCode/src/main/java/expo/subsystems/Outtake.kt
@@ -1,9 +1,18 @@
 package expo.subsystems
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.hardware.Servo
 import expo.Subsystem
 
 class Outtake : Subsystem {
+    private lateinit var linkage: Servo
     override fun initialize(opMode: LinearOpMode) {
+        linkage = opMode.hardwareMap.get(Servo::class.java, "linkage")
+        linkage.scaleRange(0.0, 0.8)
+        linkage.direction = Servo.Direction.REVERSE
+    }
+
+    fun setLinkagePosition(position: Double) {
+        linkage.position = position
     }
 }
