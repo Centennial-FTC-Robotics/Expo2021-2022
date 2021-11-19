ba66a2419d5061a10cc731ccb839871491eb715e
ramesh babu <rameshbabu.pv@gmail.com>
2021-11-18 21:43:25 -0500

    outtake class skeleton

--- TeamCode/src/main/java/expo/subsystems/Outtake.kt
+++ TeamCode/src/main/java/expo/subsystems/Outtake.kt
@@ -3,12 +3,13 @@ package expo.subsystems
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.Servo
 import expo.Subsystem
+import expo.hardware.Joint
 
 /* joint1/3:
     REST, LEFT, RIGHT, ALLIANCE(aka STRAIGHT)
    joint2:
     REST, LEFT, RIGHT, ALLIANCE3, ALLIANCE2, ALLIANCE1
- * MOVE JOINT 2 FIRST OR ELSE BAD THINGS MIGHT HAPPEN IM NOT SURE THO BUT JOHN SAID SO HES ALSO A LOSER
+ * MOVE JOINT 2 FIRST OR ELSE BAD THINGS MIGHT HAPPEN I'M NOT SURE THO BUT JOHN SAID SO HES ALSO A LOSER
  * carriage:
     - 2 positions (IN, OUT)
  * slides:
@@ -17,15 +18,38 @@ import expo.Subsystem
 
 class Outtake : Subsystem {
     private lateinit var linkage: Servo
-
+    private lateinit var carriage: Servo
+    private lateinit var joint1: Joint
+    private lateinit var joint2: Joint
+    private lateinit var weedJoke3: Joint
     override fun initialize(opMode: LinearOpMode) {
         linkage = opMode.hardwareMap.get(Servo::class.java, "linkage")
+        carriage = opMode.hardwareMap.get(Servo::class.java, "carriage")
         linkage.scaleRange(0.0, 0.8)
         linkage.direction = Servo.Direction.REVERSE
+        joint1 = Joint(0.0, 0.0, 0.0,0.0, opMode, "joint1");
+        joint2 = Joint(0.0, 0.0, 0.0,0.0, 0.0, 0.0, opMode, "joint2");
+        weedJoke3 = Joint(0.0, 0.0, 0.0,0.0, opMode, "weedJoke3");
+    }
+    fun setJoint1(position: Joint.Position){
+        joint1.setPosition(position);
+    }
+
+    fun setJoint2(position: Joint.Position){
+        joint2.setPosition(position);
+    }
+    fun setJoint3(position: Joint.Position){
+        weedJoke3.setPosition(position);
     }
 
     fun setLinkagePosition(position: Double) {
         linkage.position = position
+
+
     }
+    fun setCarriagePosition(position: Double) {
+        carriage.position = position
+    }
+
 
 }
