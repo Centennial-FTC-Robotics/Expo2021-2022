949b7e563f44e724820deaf981edd5ef359af58f
LuqDude <luqmanforge@gmail.com>
2021-11-18 20:13:31 -0500

    -john is stupid

--- TeamCode/src/main/java/expo/subsystems/Outtake.kt
+++ TeamCode/src/main/java/expo/subsystems/Outtake.kt
@@ -4,8 +4,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.Servo
 import expo.Subsystem
 
+/* joint1/3:
+    REST, LEFT, RIGHT, ALLIANCE(aka STRAIGHT)
+   joint2:
+    REST, LEFT, RIGHT, ALLIANCE3, ALLIANCE2, ALLIANCE1
+ * MOVE JOINT 2 FIRST OR ELSE BAD THINGS MIGHT HAPPEN IM NOT SURE THO BUT JOHN SAID SO HES ALSO A LOSER
+ * carriage:
+    - 2 positions (IN, OUT)
+ * slides:
+    - ill think abt this later when i actually have a brain
+ */
+
 class Outtake : Subsystem {
     private lateinit var linkage: Servo
+
     override fun initialize(opMode: LinearOpMode) {
         linkage = opMode.hardwareMap.get(Servo::class.java, "linkage")
         linkage.scaleRange(0.0, 0.8)
@@ -15,4 +27,5 @@ class Outtake : Subsystem {
     fun setLinkagePosition(position: Double) {
         linkage.position = position
     }
+
 }
