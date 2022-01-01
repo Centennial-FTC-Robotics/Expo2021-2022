4f28205b8951cda7238c63bbd4ae4ac075b5a26f
LuqDude <luqmanforge@gmail.com>
2021-12-31 21:46:47 -0500

    -made a better robot class

--- TeamCode/src/main/java/expo/Robot.kt
+++ TeamCode/src/main/java/expo/Robot.kt
@@ -1,20 +1,93 @@
 package expo
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.hardware.lynx.LynxModule
+import com.qualcomm.robotcore.util.ReadWriteFile
+import expo.command.CommandScheduler
+import expo.logger.Item
+import expo.logger.Logger
 import expo.subsystems.*
+import expo.util.ExpoOpMode
+import expo.util.Vector
+import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil
+import java.io.File
+
 
 object Robot {
     val drivetrain = Drivetrain()
+    val outtake = Outtake()
     val intake = Intake()
     val spinner = CarouselSpinner()
     val IMU = IMU()
-    val openCV = OpenCV()
     val motorTester = MotorTester()
     val odometry = Odometry()
+    val odoLifter = OdometryLifter()
+    val hubs = ArrayList<LynxModule>()
+    val wallSensors = WallSensors()
+    val CV = OpenCV()
+    lateinit var opMode: ExpoOpMode
 
-    private val subsystems = listOf(drivetrain, intake, spinner, IMU, openCV, odometry)
-    fun initialize(opMode: LinearOpMode) {
+    private val odoData: File = AppUtil.getInstance().getSettingsFile("odoData.txt")
+
+
+//    private val subsystems = listOf(intake, drivetrain, spinner, IMU, odometry, odoLifter, outtake, wallSensors)
+    private val subsystems = listOf(CV)
+    fun initialize(opMode: ExpoOpMode) {
+        this.opMode = opMode
+        hubs.addAll(opMode.hardwareMap.getAll(LynxModule::class.java))
+        CommandScheduler.reset()
+        Logger.reset()
         for (subsystem: Subsystem in subsystems)
             subsystem.initialize(opMode)
+
+        update()
+        while (CommandScheduler.instance.isRunning() && opMode.opModeIsActive()) {
+            update()
+        }
+
+        Logger.getInstance().addLine(Item.Line("Robot initialized"))
+        Logger.getInstance().update()
+    }
+
+    fun update() {
+        for (hub in opMode.hardwareMap.getAll(LynxModule::class.java)) {
+            hub.clearBulkCache()
+        }
+        CommandScheduler.instance.update()
+        Logger.getInstance().update()
+    }
+
+    fun saveOdoData() {
+        var data = ""
+        val pos: Vector = odometry.getPos()
+        val angle: Double = Math.toDegrees(odometry.getHeading())
+        data += pos.getX().toString() + "," + pos.getY().toString() + "," + angle.toString() + "," + opMode.team.name
+        ReadWriteFile.writeFile(odoData, data)
+    }
+
+    fun readOdoData() {
+        val rawData = ReadWriteFile.readFile(odoData)
+        val rawDataArr = rawData.split(",").toTypedArray()
+        try {
+            val x = rawDataArr[0].toDouble()
+            val y = rawDataArr[1].toDouble()
+            val heading = rawDataArr[2].toFloat()
+            val team = ExpoOpMode.Team.valueOf(rawDataArr[3])
+            odometry.setStartPos(x, y, Math.toRadians(heading.toDouble()))
+            if (heading > 180) {
+                var diff = heading - 180
+                diff -= 180f
+                IMU.setStartAngle(diff.toDouble())
+            } else {
+                IMU.setStartAngle(heading.toDouble())
+            }
+            opMode.team = team
+        } catch (e: NumberFormatException) {
+            odometry.setStartPos(0.0, 0.0, 0.0)
+            IMU.setStartAngle(0.0)
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            odometry.setStartPos(0.0, 0.0, 0.0)
+            IMU.setStartAngle(0.0)
+        }
     }
 }
