f819956e5893c718122c6b9865f0db7931c06efa
LuqDude <luqmanforge@gmail.com>
2022-01-01 00:25:40 -0500

    -intake exists

---
+++ TeamCode/src/main/java/expo/command/commands/IntakeCommand.kt
@@ -0,0 +1,51 @@
+package expo.command.commands
+
+import com.qualcomm.robotcore.util.ElapsedTime
+import expo.Robot
+import expo.Subsystem
+import expo.command.Command
+
+class IntakeCommand(private var state: Int) : Command {
+    override var isFinished: Boolean = false
+    private var timer = ElapsedTime()
+
+    constructor() : this(0)
+
+
+    override fun update() {
+        if (state == 0) {
+            Robot.intake.setPower(.4)
+            timer.reset()
+            state++;
+        } else if (state == 1) {
+            if (timer.milliseconds() > 500) {
+                Robot.intake.setPower(.15)
+                Robot.intake.setJointPosition(1.0)
+                state++
+                timer.reset()
+            }
+        } else if (state == 2) {
+            if (timer.milliseconds() > 1300) {
+                Robot.intake.setPower(0.0)
+                Robot.intake.setJointPosition(0.0)
+                state++
+                timer.reset()
+            } else if (timer.milliseconds() > 500) {
+                Robot.intake.setPower(0.4)
+            }
+        } else if (state == 3) {
+            if (timer.milliseconds() > 500) {
+                isFinished = true
+            }
+        }
+    }
+
+    override fun done() {
+
+    }
+
+
+    override fun requiredSubsystems(): MutableSet<Subsystem> {
+        return mutableSetOf(Robot.intake)
+    }
+}
--- TeamCode/src/main/java/expo/subsystems/Intake.kt
+++ TeamCode/src/main/java/expo/subsystems/Intake.kt
@@ -1,11 +1,25 @@
 package expo.subsystems
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.Servo
 import expo.Subsystem
 
 class Intake : Subsystem {
-    //TODO: john
+    private lateinit var intakeMotor: DcMotor
+    private lateinit var intakeJoint: Servo
     override fun initialize(opMode: LinearOpMode) {
+        intakeMotor = opMode.hardwareMap.get(DcMotor::class.java, "intake")
+        intakeJoint = opMode.hardwareMap.get(Servo::class.java, "intake servo")
+        intakeJoint.direction = Servo.Direction.REVERSE
+        intakeJoint.position = 0.0
+    }
 
+    fun setPower(power: Double) {
+        intakeMotor.power = power
+    }
+
+    fun setJointPosition(position: Double) {
+        intakeJoint.position = position
     }
 }
