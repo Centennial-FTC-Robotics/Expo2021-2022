4c605f6a6a358a6b31b93be94540f3773d6da330
LuqDude <luqmanforge@gmail.com>
2021-12-31 21:59:31 -0500

    -auto and teleops

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedWarehouseParkAuto.kt
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.auto
+
+import expo.util.ExpoOpMode
+
+class RedWarehouseParkAuto : ExpoOpMode() {
+    override fun runOpMode() {
+        super.runOpMode()
+    }
+}
